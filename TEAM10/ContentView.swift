f41d7213172b3f80e0fa2b5801b60cc8fc36b603
Nada-09 <nadaalmajidi4@gmail.com>
2024-09-30 09:45:21 +0300

    M

--- TEAM10/ContentView.swift
+++ TEAM10/ContentView.swift
@@ -1,19 +1,33 @@
 //
 //  ContentView.swift
-//  TEAM10
+//  n
 //
-//  Created by Nada Abdullah on 27/03/1446 AH.
+//  Created by Nada Abdullah on 26/03/1446 AH.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack{
+                Image("images")
+                    .resizable()
+                    . frame(width: 250,height: 250)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.red, lineWidth: 10))
+            }
+        VStack(alignment:.leading) {
+            Text("Nada Abdullah")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(Color.green)
+                
+            
+            Text("I'm 24 years old, Computer Scinece student, and a passionate programmer")
+                .font(.subheadline)
+                .fontWeight(.medium)
+                .foregroundColor(Color.gray)
+                .multilineTextAlignment(.leading)
+                
         }
         .padding()
     }
